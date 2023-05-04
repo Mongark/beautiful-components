2105bfc8a2d9795c165143576788353e3fdea70c
Mongark <mongarkprogrammer@gmail.com>
2023-05-04 13:18:32 -0400

    Created Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import MarkdownPage from './components/MarkdownPage/MarkdownPage';
+import {
+    Outlet
+} from 'react-router-dom';
 
 function App() {
-    const sample_texts = [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    ];
 
     return (
         <div className='App' >
             <Navbar />
 
             <div className='AppBody'>
-                <MarkdownPage title='Anything' content={sample_texts[0]} />
+                <Outlet />
             </div>
         </div>
     )
